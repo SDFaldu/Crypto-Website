bc9482a496c0418a8c1885bdc31baaf8be3c0a62
SDFaldu <shreelfaldu2305@gmail.com>
2023-01-23 08:09:41 +0530

    sample commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var btc = document.getElementById("bitcoin");
+
+var settings = {
+    "async": true,
+    "scrossDomain": true,  
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd",
+    "method": "GET",
+    "headers": {}
+}
+$.ajax(settings).done(function (responce){
+    btc.innerHTML = responce.bitcoin.usd
+});
